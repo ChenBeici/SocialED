24188154467af76875aef93ab914cb65a4010fa1
naruto <you@example.com>
2024-07-06 17:45:21 +0800

    updata conf

--- source/conf.py
+++ source/conf.py
@@ -21,9 +21,6 @@ release = '0.1.0'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
     'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
@@ -38,9 +35,10 @@ extensions = [
 bibtex_bibfiles = ['zreferences.bib']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 master_doc = 'index'
 source_suffix = '.rst'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -48,3 +46,67 @@ source_suffix = '.rst'
 html_theme = 'furo'
 html_static_path = ['_static']
 html_favicon = 'socialed.ico'
+
+htmlhelp_basename = 'socialeddoc'
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+latex_documents = [
+    (master_doc, 'socialed.tex', 'SocialED Documentation',
+     'socialED Team', 'manual'),
+]
+
+man_pages = [
+    (master_doc, 'socialed', 'SocialED Documentation',
+     [author], 1)
+]
+
+texinfo_documents = [
+    (master_doc, 'socialed', 'SocialED Documentation',
+     author, 'SocialED', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+from sphinx_gallery.sorting import FileNameSortKey
+
+html_static_path = []
+
+sphinx_gallery_conf = {
+    'examples_dirs': 'examples/',
+    'gallery_dirs': 'tutorials/',
+    'within_subsection_order': FileNameSortKey,
+    'filename_pattern': '.py',
+    'download_all_examples': False,
+}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/{.major}".format(sys.version_info),
+               None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+    "networkx": ("https://networkx.org/documentation/stable/", None),
+    'torch': ("https://pytorch.org/docs/master", None),
+    'torch_geometric': ("https://pytorch-geometric.readthedocs.io/en/latest",
+                        None),
+}
+
+
+
+
